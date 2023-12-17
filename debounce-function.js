dcd585322e115a8cde54f3eb022dc756339f06c9
Сергей Ляхов <serjml@Mac-Serjml.local>
2023-12-17 21:27:38 +0300

    create debounce function

---
+++ debounce-function.js
@@ -0,0 +1,7 @@
+function createDebounceFunction(func, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), delay);
+  };
+}
