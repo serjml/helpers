0bf1995f7628c2e4c68c2ef149dabcf88611613c
Сергей Ляхов <serjml@Mac-Serjml.local>
2024-02-08 21:25:30 +0300

    check object has a property

---
+++ object-has-a-property.js
@@ -0,0 +1,27 @@
+/**
+ * The function so that it returns true if the object passed to it
+ * contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+ */
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function isEveryoneHere(userObj) {
+  return ['Alan', 'Jeff', 'Sarah', 'Ryan'].every((name) => userObj.hasOwnProperty(name));
+}
