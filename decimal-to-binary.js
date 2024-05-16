0259b24ec29c178b772fd9f18eaac2573ccc9378
Сергей Ляхов <serjml@Mac-Serjml.local>
2024-05-16 21:09:42 +0300

    implement decimal to binary function

---
+++ decimal-to-binary.js
@@ -0,0 +1,7 @@
+const decimalToBinary = (input) => {
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
